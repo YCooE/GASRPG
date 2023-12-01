4e985bec2f1a26b61c3f523817c291922b0a01b4
YCooE <yilong.ivan.feng@gmail.com>
2023-12-01 18:47:27 +1100

    Initialise Ability Actor info for specified pawns

--- Source/Aura/Public/Character/AuraEnemy.h
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -17,7 +17,12 @@ class AURA_API AAuraEnemy : public AAuraCharacterBase, public IEnemyInterface
 
 public:
 	AAuraEnemy();
+
+	// Enemy interface
 	virtual void HighlightActor() override;
 
 	virtual void UnHighlightActor() override;
+	// End enemy interface
+
+	virtual void BeginPlay() override;
 };
